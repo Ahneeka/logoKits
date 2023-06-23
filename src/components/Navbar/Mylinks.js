9af31fe3c7aa2a4e5f28d5808d8875727b19bbac
nnjokunneka111@gmail.com <nnjokunneka111@gmail.com>
2023-06-23 19:22:20 +0100

    save

---
+++ src/components/Navbar/Mylinks.js
@@ -0,0 +1,173 @@
+
+export  const links = [
+    { name: "Men", submenu:true, sublinks:[
+      {
+        Head:"Topwear",
+        sublink:[
+          {name:"T-shirt", link:"/"},
+          {name:"Casual shirt", link:"/"},
+          {name:"Formal shirt", link:"/"},
+          {name:"Traditional wear", link:"/"},
+          {name:"Igbo wear", link:"/"}
+        ]
+      },
+
+      {
+        Head:"Bottomwear",
+        sublink:[
+          {name:"Trouser", link:"/"},
+          {name:"Jeans", link:"/"},
+          {name:"Cago pants", link:"/"},
+          {name:"Breeches", link:"/"},
+          {name:"Tracksuit bottom", link:"/"}
+        ]
+      },
+
+      {
+        Head:"Innerwear",
+        sublink:[
+          {name:"Boxers", link:"/"},
+          {name:"Boxer Briefs", link:"/"},
+          {name:"Trunks", link:"/"},
+          {name:"Tanga", link:"/"},
+          {name:"Jockstrap", link:"/"}
+        ]
+      },
+
+      {
+        Head:"sleep wear",
+        sublink:[
+          {name:"Linen pajamas", link:"/"},
+          {name:"Cotton pajamas", link:"/"},
+          {name:"Silk pajamas", link:"/"},
+          {name:"Fleece pajams", link:"/"},
+          {name:"Satin pajams", link:"/"}
+        ]
+      },
+
+      {
+        Head:"foot wear",
+        sublink:[
+          {name:"Oxford", link:"/"},
+          {name:"sneakers", link:"/"},
+          {name:"Loafer", link:"/"},
+          {name:"Brogues", link:"/"},
+          {name:"Mule", link:"/"}
+        ]
+      }
+
+    ] }, 
+    { name: "Women",submenu:true, sublinks:[
+      {
+        Head:"Topwear",
+        sublink:[
+          {name:"T-shirt", link:"/"},
+          {name:"Casual shirt", link:"/"},
+          {name:"Formal shirt", link:"/"},
+          {name:"Traditional wear", link:"/"},
+          {name:"Igbo wear", link:"/"}
+        ]
+      },
+
+      {
+        Head:"Bottomwear",
+        sublink:[
+          {name:"Bell bottoms", link:"/"},
+          {name:"culottes", link:"/"},
+          {name:"Harem pants", link:"/"},
+          {name:"palazzo pants", link:"/"}
+        ]
+      },
+
+      {
+        Head:"Innerwear",
+        sublink:[
+          {name:"Thong", link:"/"},
+          {name:"Bra", link:"/"},
+          {name:"Undies", link:"/"},
+          {name:"Bralette", link:"/"},
+          {name:"Chemise", link:"/"}
+        ]
+      },
+
+      {
+        Head:"sleep wear",
+        sublink:[
+          {name:"T-shirt", link:"/"},
+          {name:"Casual shirt", link:"/"},
+          {name:"Formal shirt", link:"/"},
+          {name:"Traditional wear", link:"/"},
+          {name:"Igbo wear", link:"/"}
+        ]
+      },
+
+      {
+        Head:"foot wear",
+        sublink:[
+          {name:"T-shirt", link:"/"},
+          {name:"Casual shirt", link:"/"},
+          {name:"Formal shirt", link:"/"},
+          {name:"Traditional wear", link:"/"},
+          {name:"Igbo wear", link:"/"}
+        ]
+      }
+
+    ] }, 
+    { name: " Kid's", submenu:true, sublinks:[
+      {
+        Head:"Topwear",
+        sublink:[
+          {name:"T-shirt", link:"/"},
+          {name:"Casual shirt", link:"/"},
+          {name:"Formal shirt", link:"/"},
+          {name:"Traditional wear", link:"/"},
+          {name:"Igbo wear", link:"/"}
+        ]
+      },
+
+      {
+        Head:"Bottomwear",
+        sublink:[
+          {name:"Trouser", link:"/"},
+          {name:"Jeans", link:"/"},
+          {name:"Cago pants", link:"/"},
+          {name:"Breeches", link:"/"},
+          {name:"Tracksuit bottom", link:"/"}
+        ]
+      },
+
+      {
+        Head:"Innerwear",
+        sublink:[
+          {name:"Boxers", link:"/"},
+          {name:"Boxer Briefs", link:"/"},
+          {name:"Trunks", link:"/"},
+          {name:"Tanga", link:"/"},
+          {name:"Jockstrap", link:"/"}
+        ]
+      },
+
+      {
+        Head:"sleep wear",
+        sublink:[
+          {name:"Linen pajamas", link:"/"},
+          {name:"Cotton pajamas", link:"/"},
+          {name:"Silk pajamas", link:"/"},
+          {name:"Fleece pajams", link:"/"},
+          {name:"Satin pajams", link:"/"}
+        ]
+      },
+
+      {
+        Head:"foot wear",
+        sublink:[
+          {name:"Oxford", link:"/"},
+          {name:"sneakers", link:"/"},
+          {name:"Loafer", link:"/"},
+          {name:"Brogues", link:"/"},
+          {name:"Mule", link:"/"}
+        ]
+      }
+
+    ] }
+  ];
--- src/components/Navbar/NavbarLinks.js
+++ src/components/Navbar/NavbarLinks.js
@@ -1,124 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { links } from "./Mylinks"
 
 const NavbarLinks = () => {
-  const links = [
-    { name: "Men", submenu:true, sublinks:[
-      {
-        Head:"Topwear",
-        sublink:[
-          {name:"T-shirt", link:"/"},
-          {name:"Casual shirt", link:"/"},
-          {name:"Formal shirt", link:"/"},
-          {name:"Traditional wear", link:"/"},
-          {name:"Igbo wear", link:"/"}
-        ]
-      },
-
-      {
-        Head:"Bottomwear",
-        sublink:[
-          {name:"Trouser", link:"/"},
-          {name:"Jeans", link:"/"},
-          {name:"Cago pants", link:"/"},
-          {name:"Breeches", link:"/"},
-          {name:"Tracksuit bottom", link:"/"}
-        ]
-      },
-
-      {
-        Head:"Innerwear",
-        sublink:[
-          {name:"Boxers", link:"/"},
-          {name:"Boxer Briefs", link:"/"},
-          {name:"Trunks", link:"/"},
-          {name:"Tanga", link:"/"},
-          {name:"Jockstrap", link:"/"}
-        ]
-      },
-
-      {
-        Head:"sleep wear",
-        sublink:[
-          {name:"Linen pajamas", link:"/"},
-          {name:"Cotton pajamas", link:"/"},
-          {name:"Silk pajamas", link:"/"},
-          {name:"Fleece pajams", link:"/"},
-          {name:"Satin pajams", link:"/"}
-        ]
-      },
-
-      {
-        Head:"foot wear",
-        sublink:[
-          {name:"Oxford", link:"/"},
-          {name:"sneakers", link:"/"},
-          {name:"Loafer", link:"/"},
-          {name:"Brogues", link:"/"},
-          {name:"Mule", link:"/"}
-        ]
-      }
-
-    ] }, 
-    { name: "Women",submenu:true, sublinks:[
-      {
-        Head:"Topwear",
-        sublink:[
-          {name:"T-shirt", link:"/"},
-          {name:"Casual shirt", link:"/"},
-          {name:"Formal shirt", link:"/"},
-          {name:"Traditional wear", link:"/"},
-          {name:"Igbo wear", link:"/"}
-        ]
-      },
-
-      {
-        Head:"Bottomwear",
-        sublink:[
-          {name:"Bell bottoms", link:"/"},
-          {name:"culottes", link:"/"},
-          {name:"Harem pants", link:"/"},
-          {name:"palazzo pants", link:"/"}
-        ]
-      },
-
-      {
-        Head:"Innerwear",
-        sublink:[
-          {name:"Thong", link:"/"},
-          {name:"Bra", link:"/"},
-          {name:"Undies", link:"/"},
-          {name:"Bralette", link:"/"},
-          {name:"Chemise", link:"/"}
-        ]
-      },
-
-      {
-        Head:"sleep wear",
-        sublink:[
-          {name:"T-shirt", link:"/"},
-          {name:"Casual shirt", link:"/"},
-          {name:"Formal shirt", link:"/"},
-          {name:"Traditional wear", link:"/"},
-          {name:"Igbo wear", link:"/"}
-        ]
-      },
-
-      {
-        Head:"foot wear",
-        sublink:[
-          {name:"T-shirt", link:"/"},
-          {name:"Casual shirt", link:"/"},
-          {name:"Formal shirt", link:"/"},
-          {name:"Traditional wear", link:"/"},
-          {name:"Igbo wear", link:"/"}
-        ]
-      }
-
-    ] }, 
-    { name: " Kid's" }
-  ];
-
   return (
     <>
       {links.map((link) => (
@@ -128,7 +12,7 @@ const NavbarLinks = () => {
 
             {link.submenu && (
               <div>
-                <div className="absolute top-20 hidden group-hover:block hover:blo">
+                <div className="absolute top-20 hidden group-hover:md:block hover:md:block">
                   <div className="py-3">
                     <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
                   </div>
@@ -138,8 +22,8 @@ const NavbarLinks = () => {
                         <div>
                           <h1 className="text-lg font-semibold">{mysublinks.Head}</h1>
                           {mysublinks.sublink.map(slink =>(
-                            <li classname="text-sm text-gray-600 my-2.5">
-                              <Link to={slink.name} className="hover:text-blue-600">{slink.name}</Link>
+                            <li classname="text-sm my-2.5">
+                              <Link to={slink.name} className="hover:text-blue-600  text-gray-500">{slink.name}</Link>
                             </li>
                           ))}
                         </div>
@@ -150,6 +34,8 @@ const NavbarLinks = () => {
               </div>
             )}
           </div>
+          {/* mobile menu */}
+          <div></div>
         </div>
       ))}
     </>
